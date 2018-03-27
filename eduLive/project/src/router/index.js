9f44a0209a36fd04e9049639e08f5fd0fa3d4599
Your Name <478855960@qq.com>
2018-03-27 18:14:32 +0800

    教师直播间前端界面框架初步实现 #27

--- eduLive/project/src/router/index.js
+++ eduLive/project/src/router/index.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import Axios from 'axios'
 import HelloWorld from '@/components/HelloWorld'
+import LiveTeacher from '@/components/Live_Teacher'
 
 Vue.use(Router)
+Vue.use(ElementUI)
+Vue.prototype.$ajax = Axios
 
 export default new Router({
   routes: [
@@ -10,6 +16,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/live_teacher',
+      name: 'Live_Teacher',
+      component: LiveTeacher
     }
   ]
 })
